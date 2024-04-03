246062aeef8b387f23cc05192742fa91c4f9bf26
TerrelBriggs <terrel.briggs11@gmail.com>
2024-04-03 03:23:58 +0000

    basic models implementation

--- App/models/user.py
+++ App/models/user.py
@@ -5,6 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
+    #routines = db.relationship('Routine', backref='user') ... this doesn't work, not sure if it's needed
 
     def __init__(self, username, password):
         self.username = username
@@ -15,6 +16,20 @@ class User(db.Model):
             'id': self.id,
             'username': self.username
         }
+
+    def create_routine(self, name, duration):
+        exercise = Workout.query.get(workout_id)
+        if exercise:
+            try:
+                workout = Routine(self.id, workout_id, name, duration)
+                db.session.add(workout)
+                db.session.commit()
+                return workout
+            except Exception as e:
+                print(e)
+                db.session.rollback()
+                return None
+        return None
 
     def set_password(self, password):
         """Create hashed password."""
