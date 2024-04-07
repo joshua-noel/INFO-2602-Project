5e510f519b84e6842f08960f53217cda2f8d52ae
Joshua Noel <noelejoshua@gmail.com>
2024-04-07 00:58:37 +0000

    workout controllers added

---
+++ App/controllers/workout.py
@@ -0,0 +1,30 @@
+from App.models import Workout
+from App.database import db
+
+def get_all_workouts():
+    return Workout.query.all()
+
+def get_workout_by_name(name):
+    return Workout.query.filter_by(name=name).first()
+
+def get_all_workouts_json():
+    workouts = get_all_workouts()
+
+    if not workouts:
+        return []
+
+    return [workout.get_json() for workout in workouts]
+
+def update_workout(name, set_count, rep_count):
+    workout = get_workout_by_name(name)
+
+    if workout:
+        workout.name = name
+        workout.set_count = set_count
+        workout.rep_count = rep_count
+        db.session.add(workout)
+        return db.session.commit()
+
+    return None
+    
+    
