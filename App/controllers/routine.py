76813629f6e279d8b074037761d9af173d8e17fa
Joshua Noel <noelejoshua@gmail.com>
2024-04-08 01:14:06 +0000

    added routine  controller

--- App/controllers/routine.py
+++ App/controllers/routine.py
@@ -1,2 +1,31 @@
 from App.models import routine
 from App.database import db
+
+def get_all_routines():
+    return Routine.query.all()
+
+def get_routine_by_name(name):
+    return Routine.query.filter_by(name=name).first()
+
+def get_all_routines_json():
+    routines = get_all_routines()
+
+    if not routines:
+        return []
+
+    return [routine.get_json() for routine in routines]
+
+def create_routine(name, duration):
+    routine = Routine(name=name, duration=duration)
+    db.session.add(workout)
+    return db.session.commit()
+
+def update_routine(name, duration):
+    routine = get_routine_by_name(name)
+
+    if routine:
+        routine.duration = duration
+        db.session.add(workout)
+        return db.session.commit()
+
+    return None
